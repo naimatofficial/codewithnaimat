44d7da5cfc947c445af863747296a347742dc72b
Naimat Ullah <naimat.official.10@gmail.com>
2022-03-10 19:41:35 +0500

    .

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="mt-8 flex w-full justify-center bg-gray-200 p-12 text-xl font-bold text-gray-600">
+        <p> Created By Naimat Ullah with ❤</p>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { Layout } from '../components'
 import '../styles/global.scss'
 import type { AppProps } from 'next/app'
+import { Footer } from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Footer />
     </Layout>
   )
 }
