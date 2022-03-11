6b05c6deb2c9fc4d75a6da5cad1edbe25c625e0b
Naimat Ullah <naimat.official.10@gmail.com>
2022-03-11 10:28:33 +0500

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <div className="mt-8 flex w-full justify-center bg-gray-200 p-12 text-xl font-bold text-gray-600">
+      <div className=" mt-8 flex w-full justify-center bg-gray-200 p-12 text-xl font-bold text-gray-600">
         <p> Created By Naimat Ullah with ❤</p>
       </div>
     </div>
